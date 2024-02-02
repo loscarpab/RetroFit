605c7fc1dc705d84c1c6d78752b8e7c41741773e
loscarpab <ccormor392@g.educaand.es>
2024-02-02 19:04:18 +0100

    comentado

--- app/src/main/java/com/ccormor392/retrofit/mainScreen/network/GitHubApiService.kt
+++ app/src/main/java/com/ccormor392/retrofit/mainScreen/network/GitHubApiService.kt
@@ -6,15 +6,15 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-
+// Definición de la URL base para las solicitudes a la API de GitHub
 private const val BASE_URL = "https://api.github.com/"
-
+// Creación de una instancia de Retrofit utilizando el patrón de diseño Builder
 val retrofit = Retrofit.Builder()
-    .addConverterFactory(GsonConverterFactory.create())
-    .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())// Configuración para usar Gson para la conversión JSON
+    .baseUrl(BASE_URL)// Establece la URL base para todas las solicitudes
     .build()
-
+// Definición de una interfaz que especifica los métodos para interactuar con la API de GitHub
 interface GitHubApiService{
-    @GET("users")
-    fun getUsers():Call<List<User>>
+    @GET("users")// Anotación indicando que este método realizará una solicitud HTTP GET a la ruta "users"
+    fun getUsers():Call<List<User>>// Método para obtener una lista de usuarios, devuelve un objeto Call<List<User>>
 }
--- app/src/main/java/com/ccormor392/retrofit/mainScreen/ui/UserViewModel.kt
+++ app/src/main/java/com/ccormor392/retrofit/mainScreen/ui/UserViewModel.kt
@@ -10,29 +10,40 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+// ViewModel utilizado para gestionar y proporcionar datos relacionados con los usuarios
 class UserViewModel : ViewModel() {
+
+    // MutableLiveData que contendrá la lista de usuarios; se utiliza para actualizaciones en tiempo real
     private val _users = MutableLiveData<List<User>>()
     val users: LiveData<List<User>> get() = _users
 
+    // Método para cargar la lista de usuarios desde la API de GitHub
     fun loadUsers() {
+        // Creación de una instancia del servicio GitHubApiService utilizando la instancia de Retrofit creada previamente
         val service = retrofit.create(GitHubApiService::class.java)
+
+        // Creación de una llamada asíncrona para obtener la lista de usuarios
         val call = service.getUsers()
 
+        // Manejo de la respuesta asíncrona utilizando enqueue
         call.enqueue(object : Callback<List<User>> {
+            // Método llamado cuando la respuesta HTTP es exitosa
             override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
                 if (response.isSuccessful) {
+                    // Obtiene la lista de usuarios de la respuesta y la asigna al MutableLiveData
                     val userList = response.body()
                     _users.value = userList!!
                 } else {
-
+                    // Puedes manejar aquí el caso en el que la respuesta no sea exitosa
                 }
             }
-
+            // Método llamado cuando la solicitud HTTP falla
             override fun onFailure(call: Call<List<User>>, t: Throwable) {
-
+                // Puedes manejar aquí el caso en el que la solicitud falle
             }
         })
     }
 }
+
 
 
